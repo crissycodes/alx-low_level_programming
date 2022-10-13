382a35b87cc8c025858df9b7dce80349a9c22d46
christopher kada <crissykada@gmail.com>
2022-10-13 14:46:41 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+* main - print the string in the put function
+*
+*
+* this program prints with proper grammar, but the outcome is a piece of art,
+*
+* Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
