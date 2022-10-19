f048eddaba034aa89c8ccba7b93ad707dafd7833
christopher kada <crissykada@gmail.com>
2022-10-19 14:40:44 -0700

    added advanced tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half, half1, half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2, half1, half2;
 	
 	for (count = 0; count < 92; count++)
 	{
@@ -27,7 +27,7 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
