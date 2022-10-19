f770608d6808d7b98601b4b0823c2f5a2a99ff01
christopher kada <crissykada@gmail.com>
2022-10-19 15:11:21 -0700

    added 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 0; i < 2024; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 		{
-			sum += sum;
+			sum += i;
 		}
 	}
 	printf("%d\n", sum);
