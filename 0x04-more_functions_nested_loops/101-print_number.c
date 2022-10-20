4f0dc57a1deb9a05732104082fcba2bf1a33ea02
christopher kada <crissykada@gmail.com>
2022-10-20 15:02:27 -0700

    added  101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,17 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	_putchar(num + '0');
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	} else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
 }
