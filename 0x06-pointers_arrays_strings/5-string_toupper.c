43f0fb596600b71a92dac36bb8b5183e8f70f469
christopher kada <crissykada@gmail.com>
2022-10-27 05:42:18 -0700

    updated 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (((s + count) >= 97) && ((s + count) <= 122))
+		if (((s + count) >= 'a') && ((s + count) <= 'z'))
 			*(s + count) = *(s + count) - 32;
 		count++;
 	}
